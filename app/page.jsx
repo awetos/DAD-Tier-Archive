3b901df86d51b7d138351317df83ece97e58458a
awetos <78708899+awetos@users.noreply.github.com>
2026-01-11 03:17:00 -0500

    Update Test.

--- app/page.jsx
+++ app/page.jsx
@@ -23,12 +23,7 @@ export default function Page() {
                 <ContextAlert className="mb-6" />
                 <h1 className="mb-4">Netlify Platform Starter – Next.js</h1>
                 <p className="mb-6 text-lg">
-                    Deploy the latest version of Next.js — including Turbopack, React Compiler, and the new caching APIs
-                    — on Netlify in seconds. No configuration or custom adapter required.
-                </p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
-                </Link>
+                    I am an update. </p>
             </section>
             {!!ctx && (
                 <section className="flex flex-col gap-4">

55cb55d981884cce9afc98d1b03b03e9dcd83559
awetos <78708899+awetos@users.noreply.github.com>
2026-01-16 00:57:14 -0500

    removed headers

--- app/actions/piss/leavePiss.jsx
+++ app/actions/piss/leavePiss.jsx
@@ -1,10 +1,7 @@
 'use server'
 import { neon } from "@netlify/neon"
-import { headers } from "next/headers";
-
 export default async function leavePiss(prevState, formData){
     const sql = neon();
-    console.log(headers());
 
     return { status : "You peed here!"}
 

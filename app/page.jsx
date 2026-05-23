5eb9ade27bef051d72e7c1cfa372c09c48291d9c
awetos <awetosprouts@gmail.com>
2026-05-23 16:43:14 -0500

    fixed @ utils

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { getNetlifyContext } from 'utils';
-export const dynamic = "force-dynamic";
+import { getNetlifyContext } from '@/utils';
+export const dynamic = 'force-dynamic';
 
 import Entry from '@/components/templates/entry';
 import classes from './homepage.module.css';
@@ -9,43 +9,34 @@ import getTierlists from '@/app/actions/browse/getTierlists';
 const ctx = getNetlifyContext();
 
 export default async function Page() {
-  //assume never empty. otherwise, see Pending for how to fix.
-  
+    //assume never empty. otherwise, see Pending for how to fix.
 
     const allEntries = await getTierlists();
-      let renderable = <></>
-      let no_entries = <></>
-      if(!allEntries || allEntries.length === 0 ){
-        renderable = <></>
-        no_entries = <p>No entries submitted.</p>
-      }
-      else{
-        renderable = allEntries.map((entry)=>ReturnEntry(entry))
-      }
-      
-    return (
-        <div> 
-            <div className={classes['interior-content']}> 
-          <div className={classes['centered-content']}> 
-            <p>View past tierlists made for DAD</p>
-            {no_entries}
-          </div>
-         <div className={classes['image-grid']}> 
+    let renderable = <></>;
+    let no_entries = <></>;
+    if (!allEntries || allEntries.length === 0) {
+        renderable = <></>;
+        no_entries = <p>No entries submitted.</p>;
+    } else {
+        renderable = allEntries.map((entry) => ReturnEntry(entry));
+    }
 
-          {renderable}
-            
-           
+    return (
+        <div>
+            <div className={classes['interior-content']}>
+                <div className={classes['centered-content']}>
+                    <p>View past tierlists made for DAD</p>
+                    {no_entries}
+                </div>
+                <div className={classes['image-grid']}>{renderable}</div>
             </div>
-        </div>
-    
         </div>
     );
 }
 
-function ReturnEntry(data){
-  if((data.id &&  data.image_url)
-  ){
-    return <Entry key={data.id} data={data}/>
-  }
-  return <div key={data.id}></div>
+function ReturnEntry(data) {
+    if (data.id && data.image_url) {
+        return <Entry key={data.id} data={data} />;
+    }
+    return <div key={data.id}></div>;
 }
